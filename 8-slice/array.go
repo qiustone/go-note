7135a46ac1d018c89714379a99bf00aa1c55b2ed
Mr.Wang <letshi@163.com>
2022-04-12 23:49:22 +0800

    8-slice

--- 8-slice/array.go
+++ 8-slice/array.go
@@ -2,6 +2,12 @@ package main
 
 import "fmt"
 
+func printArray(myArray [4]int) {
+	for index, value := range myArray {
+		fmt.Println("index = ", index, " value = ", value)
+	}
+}
+
 func main() {
 	//固定长度的数组
 	var myArray1 [10]int
@@ -20,4 +26,8 @@ func main() {
 	fmt.Printf("myArray1 types = %T\n", myArray1)
 	fmt.Printf("myArray2 types = %T\n", myArray2)
 	fmt.Printf("myArray2 types = %T\n", myArray3)
+
+	//printArray(myArray1) 因为数据长度也是类型的一部分，所以类型不匹配
+	printArray(myArray3)
+
 }
