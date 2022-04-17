16af0c386f88ca2a6c3a64db71bc248663a6eac0
Mr.Wang <letshi@163.com>
2022-04-17 23:57:01 +0800

    map

--- 9-map/map.go
+++ 9-map/map.go
@@ -3,6 +3,7 @@ package main
 import "fmt"
 
 func main() {
+	//============= 第一种声明方式 ================
 	//声明map类型, key和value都是string类型
 	var myMap map[string]string
 
@@ -18,4 +19,20 @@ func main() {
 	myMap["three"] = "python"
 
 	fmt.Println(myMap)
+
+	//============= 第二种声明方式 ================
+	myMap2 := make(map[int]string)
+	myMap2[1] = "java"
+	myMap2[2] = "c++"
+	myMap2[3] = "python"
+
+	fmt.Println(myMap2)
+
+	//============= 第三种声明方式 ================
+	myMap3 := map[string]string{
+		"one":   "php",
+		"two":   "c++",
+		"three": "python",
+	}
+	fmt.Println(myMap3)
 }
