f95e9827a459239199d1efdb8c2700fc641d0dea
Mr.Wang <letshi@163.com>
2022-04-18 17:59:23 +0800

    oop

---
+++ 10-struct/oop.go
@@ -0,0 +1,50 @@
+package main
+
+import "fmt"
+
+type Human struct {
+	name string
+	sex  string
+}
+
+func (this *Human) Eat() {
+	fmt.Println("Human.Eat()...")
+}
+
+func (this *Human) Walk() {
+	fmt.Println("Human.Walk()...")
+}
+
+type SuperMan struct {
+	Human //SuperMan 类继承了Human 类的方法
+	level int
+}
+
+//重定义父类的方法Eat()
+func (this *SuperMan) Eat() {
+	fmt.Println("SuperMan.Eat()...")
+}
+
+//子类的新方法
+func (this *SuperMan) Fly() {
+	fmt.Println("SuperMan.Fly()...")
+}
+
+func main() {
+	h := Human{"zhang3", "female"}
+	h.Eat()
+	h.Walk()
+
+	fmt.Println("=======================")
+
+	//定义一个子类的对象
+	//s := SuperMan{Human{"li4", "female"}, 88}
+	var s SuperMan
+	s.name = "li4"
+	s.sex = "female"
+	s.level = 88
+
+	s.Walk() //父类的方法
+	s.Eat()  //子类的方法
+	s.Fly()  //子类的方法
+}
